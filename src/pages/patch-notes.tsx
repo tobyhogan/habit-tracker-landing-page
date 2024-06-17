fda089a7df4b48f34800c12885de29ddf9bdfa16
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-17 22:26:34 +0100

    .

---
+++ src/pages/patch-notes.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link, HeadFC, PageProps } from "gatsby"
+
+
+export const PatchNotes: React.FC<PageProps> = () => {
+  return (
+    <main>
+      <h1>hi</h1>
+    </main>
+  )
+}
+
+export default PatchNotes
+
---
+++ src/pages/submit-feedback.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link, HeadFC, PageProps } from "gatsby"
+
+
+const SubmitFeedback: React.FC<PageProps> = () => {
+  return (
+    <main>
+      <h1>hi</h1>
+    </main>
+  )
+}
+
+export default SubmitFeedback
+
+
