acc4f4f8b77890dcf00edfecb0bff01238f19b3d
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-17 23:17:52 +0100

    .

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ import '../styles/index.css'
 
 export const NavBar = ({}) =>
     <nav className="flex justify-between border-b-black border-2 bg-gray-250">
-        <a className='text-[35px] ml-10 py-3' href="">Habitazen</a>
+        <a href='/' className='text-[35px] ml-10 py-3' href="">Habitazen</a>
         <ul className="flex my-auto">
             <li><a href="https://habit-tracker-indol-ten.vercel.app/">Log In / Sign Up</a></li>
             <li><a href="/">Home</a></li>
