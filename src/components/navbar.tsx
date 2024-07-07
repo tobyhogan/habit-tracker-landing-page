17a178706aef40c44552208a93eec53e692cce29
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-07 15:36:44 +0100

    .

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ export const NavBar = ({}) =>
         <a href='/' className='text-[35px] ml-10 py-3'>Habitazen</a>
         <ul className="flex my-auto">
             <li><a href="https://habit-tracker-indol-ten.vercel.app/">Log In / Sign Up</a></li>
-            <li><a href="/">Home</a></li>
+            <li><a href="/habit-tracker-landing-page">Home</a></li>
             <li><a href="/submit-feedback">Submit Feedback</a></li>
         </ul>
     </nav>
