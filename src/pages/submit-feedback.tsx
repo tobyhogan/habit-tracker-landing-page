8e56f70e26d760292b1258ad4483fc762bb1fcd3
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-19 22:56:10 +0100

    .

--- src/pages/submit-feedback.tsx
+++ src/pages/submit-feedback.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
-import { Header } from "../components/header"
+import Header from "../components/header"
 
 import '../styles/global.css'
 import '../styles/index.css'
