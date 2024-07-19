531c6ce8458cb93a67d7ef5abc5899537836f0a9
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-19 17:51:09 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,19 +35,21 @@ const IndexPage: React.FC<PageProps> = () => {
       <NavBar/>
       <main className="pb-20">
         <img src={tablescreenshot} className="w-96 border-2 mt-8"></img>
-        <h1 className="text-2xl text-center mt-8">Habitazen: Your New Favourite Habit Tracker 1234</h1>
+        <h1 className="text-2xl mt-8">Habitazen: Your New Favourite Habit Tracker</h1>
         <div className="flex flex-col mx-auto w-fit mt-8">
           <a href="https://habit-tracker-indol-ten.vercel.app/">
             <button className='rounded-md py-2 px-3 text-white bg-slate-450 hover:bg-slate-500'>Sign Up Now!</button>
           </a>
           <a href="https://habit-tracker-indol-ten.vercel.app/" className="mt-4 underline text-blue-400 mx-auto">Or Log In</a>
         </div>
-        <h1 className="text-center text-xl mt-8">Features:</h1>
+        <h2 className="text-xl mt-8">Features:</h2>
         <ul className="mx-auto w-fit mt-3 pl-8">
           <li>- Shows you habit week view</li>
           <li>- Dark Mode</li>
           <li>- Allows habit archiving</li>
         </ul>
+        <h2 className="text-xl mt-10">Download App</h2>
+        <a href="" className="">Link</a>
       </main>
     </>
   )
