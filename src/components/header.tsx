2ea6afcd5c7e6cde5fbd3de11bee187995498e00
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-19 22:39:41 +0100

    .

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { MdMenu } from "react-icons/md";
 
 import '../styles/global.css'
 import '../styles/index.css'
 
 
-export const Header = ({}) =>
+
+function Header() {
+
+  const [navOpen, setNavOpen] = useState(false)
+
+  const navList = 
+    <>
+    
+      <a href="/" className="hover:underline">Page 1</a>
+      <a href="/" className="hover:underline">Page 2</a>
+      <a href="/" className="hover:underline">Page 3</a>
+
+    </>
+
+
+  return (
+
     <nav className="NavBar flex justify-between bg-slate-100 text-slate-600">
-        <a href='/' className='text-[35px] ml-10 py-3'>Habitazen</a>
-        <ul className="flex my-auto">
-            <li><a href="https://habit-tracker-indol-ten.vercel.app/">Log In / Sign Up</a></li>
-            <li><a href="/habit-tracker-landing-page">Home</a></li>
-            <li><a href="/submit-feedback">Submit Feedback</a></li>
+      <a href='/' className='text-[35px] ml-14 py-3'>Project Title</a>
+
+      <div className="FullNav">
+
+        <ul className="flex mt-6 mr-8">
+          {navList}
         </ul>
+
+      </div>
+      
+      <div className="ToggleNav">
+
+        <button onClick={() => {setNavOpen(navOpen => !navOpen)}}>
+          <MdMenu className="ml-4 mr-8 mt-6" size={30}/>
+        </button>
+
+        {navOpen ?
+
+          <div className="border-1 border-slate-300 border-2 pt-4 pb-3 pl-2 pr-4 -ml-16 absolute rounded-md bg-white">
+            <ul className="flex flex-col text-start ml-3 [&>a]:mb-1">
+              {navList}
+            </ul>
+          </div>
+
+        : null }
+
+      </div>
+        
     </nav>
+
+  )
+
+}
+
+export default Header
