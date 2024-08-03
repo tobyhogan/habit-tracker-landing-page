b4c8c346a6c914d17926fc8b402c90ffbba0ea15
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-08-03 11:44:05 +0100

    .

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,28 +10,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+
+        
         trackingIds: [ "G-JMT5E6KQ92", ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
+
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: false,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "https://tobyhogan.github.io/habit-tracker-landing-page/",
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
-    },},},
+
+          head: true,
+
+        },
+    
+    },},
     'gatsby-plugin-postcss',
     {
       resolve: 'gatsby-plugin-manifest',
