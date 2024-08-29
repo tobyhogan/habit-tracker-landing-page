0365829a4aeffa956336688f1cdd852131952173
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-08-29 11:20:40 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,4 +51,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Landing Page</title>
+export const Head: HeadFC = () => <title>Habitazen Habit Tracker</title>
