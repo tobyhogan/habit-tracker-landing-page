8d99dec2a8f9617eb41d92abc4c12d17ad7f9c44
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-08-31 14:32:51 +0100

    .

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -121,7 +121,7 @@ function Header() {
     <header className="NavBar flex justify-between fixed w-full">
       <Link to='/' className='Link1 text-[35px] ml-14 py-2.5'>Habitazen</Link>
       <div className="flex flex-row">
-        <a href="https://habit-tracker-indol-ten.vercel.app/" className="Link1 rounded-md text-md my-auto mr-8 py-1.5 px-6 h-fit bg-gradient-to-tr from-green-500 to-blue-700 hover:from-green-600 hover:to-blue-800">
+        <a href="https://habit-tracker-indol-ten.vercel.app/" className="Link1 rounded-md text-md my-auto mr-[3vw] py-1.5 px-[3vw] h-fit bg-gradient-to-tr from-green-500 to-blue-700 hover:from-green-600 hover:to-blue-800">
           Sign Up Now!
         </a>
 
@@ -129,7 +129,7 @@ function Header() {
 
       <div className="FullNav">
 
-        <ul className="flex mt-6 mr-4">
+        <ul className="flex mt-6 mr-">
           {navList}
         </ul>
 
