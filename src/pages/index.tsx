152edcf91d49d60904e9ab8efd0c184cab3cdbff
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-09-01 21:14:06 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ const IndexPage: React.FC<PageProps> = () => {
 
           </div>
 
-          <h2 className="mt-8">What's it for?</h2>
+          <h2 className="mt-8">What does it do?</h2>
           <h3 className="mt-3">Ever struggle to stick to good habits consistently? - That's what the tracker's for!</h3>
           <h3 className="mt-3"> We've found that seeing your overall progress, and building up a streak is massively helpful in motivating you to keep up
           with good habits. </h3>
